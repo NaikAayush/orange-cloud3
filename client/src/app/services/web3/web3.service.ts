587b1fa68df6c2f6551d220dcda592459ecb9886
Samyak S Sarnayak <samyak201@gmail.com>
2021-10-09 23:50:18 +0530

    feat(ui): add wrapper for contract fns, web3 integration

--- client/src/app/services/web3/web3.service.ts
+++ client/src/app/services/web3/web3.service.ts
@@ -1,13 +1,29 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
 import Web3 from 'web3';
+import { Contract } from 'web3-eth-contract';
 
 @Injectable({
   providedIn: 'root',
 })
 export class Web3Service {
-  private web3: Web3;
+  public web3: Web3;
+  private contractAddress: string;
+  public contract: Contract;
 
   constructor() {
-    this.web3 = new Web3('web3-provider-url');
+    this.web3 = new Web3(environment.blockchainUrl);
+    this.contractAddress = environment.contractAddress;
+    this.contract = new this.web3.eth.Contract(environment.contractAbi as any, this.contractAddress);
+  }
+
+  public getAddress(): string {
+    const addr = (this.web3.currentProvider as any).selectedAddress;
+
+    if (addr === null || addr === undefined || !addr) {
+      console.log("Could not get address. Got: ", addr);
+    }
+
+    return addr;
   }
 }
