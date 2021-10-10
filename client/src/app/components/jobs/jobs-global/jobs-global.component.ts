954d56abc6e151d7e66c0602938315cd24034f95
NaikAayush <ayush@pesu.pes.edu>
2021-10-10 23:05:24 +0530

    client: barely working jobs

--- client/src/app/components/jobs/jobs-global/jobs-global.component.ts
+++ client/src/app/components/jobs/jobs-global/jobs-global.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { TheGraphService } from 'src/app/services/the-graph/the-graph.service';
 
 @Component({
   selector: 'app-jobs-global',
   templateUrl: './jobs-global.component.html',
-  styleUrls: ['./jobs-global.component.css']
+  styleUrls: ['./jobs-global.component.css'],
 })
 export class JobsGlobalComponent implements OnInit {
+  constructor(private theGraph: TheGraphService) {}
 
-  constructor() { }
-
-  ngOnInit(): void {
+  async ngOnInit() {
+    const x = await this.theGraph.getAvailableJobs();
+    console.log(x);
   }
-
 }
