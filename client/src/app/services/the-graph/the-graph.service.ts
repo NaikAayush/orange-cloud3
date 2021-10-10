1f31ece53e10596c0b724c8935b729c8f2bcd832
Samyak S Sarnayak <samyak201@gmail.com>
2021-10-10 21:55:03 +0530

    feat(ui): add the graph, core logic service
    Co-authored-by: Pranav Kesavarapu <psiayn@users.noreply.github.com>

--- client/src/app/services/the-graph/the-graph.service.ts
+++ client/src/app/services/the-graph/the-graph.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
 import { Apollo, gql } from 'apollo-angular';
 
+export interface Job {
+  id: string;
+  name: string;
+  type_: string;
+  cid: string;
+  available: boolean;
+  acceptedBy: string;
+  outputCid: string;
+  numCpus: number;
+  memBytes: number;
+}
+
+interface AvailableJobs {
+  jobEntities: Job[];
+}
+
+interface SingleJob {
+  jobEntities: Job;
+}
+
 @Injectable({
   providedIn: 'root'
 })
 export class TheGraphService {
   QUERY_GET_AVAILABLE_JOBS = gql`
-  {
+  query getAvailableJobs{
     jobEntities(first: 5, where:{available: true}) {
       id
       name
@@ -22,7 +42,7 @@ export class TheGraphService {
   `;
 
   QUERY_GET_GIVEN_JOB = gql`
-  query($id: ID!) {
+  query getJobByID($id: ID!) {
     jobEntities(where:{id: $id}) {
       id
       name
@@ -37,5 +57,28 @@ export class TheGraphService {
   }
   `;
 
+  loading: boolean = false;
+
   constructor(private apollo: Apollo) {}
+
+  async getAvailableJobs() {
+    this.loading = true;
+    const results = await this.apollo.query<AvailableJobs>({
+      query: this.QUERY_GET_AVAILABLE_JOBS,
+    }).toPromise();
+    this.loading = false;
+    return results;
+  }
+
+  async getJobByID(id: string) {
+    this.loading = true;
+    const results = await this.apollo.query<SingleJob>({
+      query: this.QUERY_GET_GIVEN_JOB,
+      variables: {
+        id: id,
+      },
+    }).toPromise();
+    this.loading = false;
+    return results;
+  }
 }
