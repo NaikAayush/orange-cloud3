322f397ce15418b7c05536f9a0a2e5c2cbd1d605
NaikAayush <ayush@pesu.pes.edu>
2021-10-10 22:12:17 +0530

    client: new ui auth mnemonic

--- client/src/app/components/sidebar/sidebar/sidebar.component.ts
+++ client/src/app/components/sidebar/sidebar/sidebar.component.ts
@@ -15,7 +15,7 @@ export class SidebarComponent implements OnInit {
     },
     {
       name: 'Settings',
-      route: '/settings',
+      route: '/auth',
       svg: 'M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4',
     },
   ];
