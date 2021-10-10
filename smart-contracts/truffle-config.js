534b988ae76f79a43a930d769686cc67bbefd176
Samyak S Sarnayak <samyak201@gmail.com>
2021-10-11 03:17:00 +0530

    feat(contract): matic deployment

--- smart-contracts/truffle-config.js
+++ smart-contracts/truffle-config.js
@@ -18,10 +18,10 @@
  *
  */
 
-// const HDWalletProvider = require('@truffle/hdwallet-provider');
-//
-// const fs = require('fs');
-// const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+const fs = require('fs');
+const mnemonic = fs.readFileSync(".secret").toString().trim();
 
 module.exports = {
   /**
@@ -45,6 +45,13 @@ module.exports = {
      host: "127.0.0.1",     // Localhost (default: none)
      port: 8550,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
+    },
+    matic: {
+      provider: () => new HDWalletProvider(mnemonic, `https://matic-mumbai.chainstacklabs.com/`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
     // Another network with more advanced options...
     // advanced: {
